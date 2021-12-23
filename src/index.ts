99a67a364e7002a282baa0ca13e6c890e4de0b23
RealNarukyn <alexmusu99@gmail.com>
2021-12-24 00:13:20 +0100

    done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hey, this is working fine');
+window.onload = () => {
+  const canvas = document.getElementById('canvas-game') as HTMLCanvasElement;
+  const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  let lastFrame = 0;
+  const render = (time: number): void => {
+    const delta = (time - lastFrame) / 1000;
+    lastFrame = time;
+    console.log(delta);
+
+    // -- Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.save();
+
+    ctx.fillStyle = 'red';
+    ctx.lineWidth = 4;
+
+    const position = { x: canvas.width / 2 - 50, y: canvas.height / 2 - 50 };
+    ctx.rect(position.x, position.y, 100, 100);
+
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.restore();
+
+    window.requestAnimationFrame(render);
+  };
+
+  window.requestAnimationFrame(render);
+};
